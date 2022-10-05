71bef3614037cc56ea7d961c20b3b2d9956f0569
tblackworkshop <tylerblack1793@gmail.com>
2022-10-05 10:11:10 -0500

    Added light/dark mode switch Added light mode styling Adjusted hamburger menu spacing and styling Adjusted nav bar styling Added smooth transitions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
+const switchElemement = document.querySelector('.switch')
+var isDark = true
+
+switchElemement.addEventListener('click', () => {
+        var body = document.querySelector('body')
+        var landing = document.getElementById("landing")
+        var contactMe = document.getElementById("contact-me")
+        var about = document.getElementById("about-me")
+        var nav = document.getElementById("nav")
+        var navList = document.getElementById("nav-list")
+
+        body.classList.toggle('light')
+        landing.classList.toggle("light-landing")
+        contactMe.classList.toggle("light-contact-me")
+        about.classList.toggle("light-about-me")
+        nav.classList.toggle("light-nav")
+        navList.classList.toggle("light-nav")
+        
+        toggleIsDark()
+        
+     })
+
 
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
+function toggleIsDark() {
+    isDark = !isDark
+}
+
 hamburgerButton.addEventListener('click', toggleButton)
+
